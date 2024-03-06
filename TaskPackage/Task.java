f2897ec31b37df8dfff17f690dd3e3bb0ba7c249
mammadkhafan <sharifrazavian@gmail.com>
2024-03-06 13:10:39 +0330

    initialize the classes and Main file

---
+++ TaskPackage/Task.java
@@ -0,0 +1,11 @@
+package TaskPackage;
+
+public class Task {
+    public String name ;
+    public double start_time ;
+    public double finish_time ;
+    
+    public double getDuration(){
+
+    }
+}
---
+++ UserPackage/User.java
@@ -0,0 +1,22 @@
+package UserPackage;
+
+public class User {
+    private String username ;
+    private String password ;
+
+    public String first_name ;
+    public String last_name ;
+    public String email ;
+    public int streak;
+
+    public Task createTask() {
+
+    }
+    
+    public String getFullName() {
+
+    }
+
+
+
+}
